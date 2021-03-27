cca3c90deba2ee132ae26feca6e3b9a1a3142314
YayL <75476395+YayLOfficial@users.noreply.github.com>
2021-03-27 18:40:39 +0100

    Update RPS.py
    Added some comments to the code

--- RPS.py
+++ RPS.py
@@ -14,25 +14,25 @@ class RPS:
         self.player_choice = ''
         self.play()
 
-    def askPlayer(self):
+    def askPlayer(self): # Ask the player what they want to pick this round and put the first letter (R, P or S) as the value for player choice
         self.player_choice = input('Choose between Rock(R), Paper(P) and Scissor(S)')[0].lower()
 
-    def choiceToInt(self):
+    def choiceToInt(self): # Turn the choice into its numerical counter part
         return 1 if self.player_choice == 'r' else 2 if self.player_choice == 'p' else 3
 
-    def intToChoice(self, choice):
+    def intToChoice(self, choice): # Reverse the choiceToInt function 
         return 'rock' if choice == 1 else 'paper' if choice == 2 else 'scissor'
 
-    def differenceToWinner(self, compChoice):
+    def differenceToWinner(self, compChoice): # Get the winner by using a trick of subtraction
         diff = self.choiceToInt() - compChoice
-        if diff == 0:
-            return f'It is a draw, the computer also picked {self.intToChoice(compChoice)}.' \
+        if diff == 0: # If the value is 0 then it is always a draw
+            return f'It is a draw, the computer also picked {self.intToChoice(compChoice)}.' \ 
                    f' {self.totalRounds-self.roundsRemaining}/{self.totalRounds}'
-        elif diff == -2 or diff == 1:
+        elif diff == -2 or diff == 1: # If the value is -2 or 1 then the player always win
             self.player_score += 1
             return f'You won, the computer picked {self.intToChoice(compChoice)}.' \
                    f' {self.totalRounds-self.roundsRemaining}/{self.totalRounds}'
-        else:
+        else: # The only other instance is that where the computer wins
             self.computer_score += 1
             return f'You lost, the computer picked {self.intToChoice(compChoice)}.' \
                    f' {self.totalRounds-self.roundsRemaining}/{self.totalRounds}'
@@ -40,11 +40,11 @@ class RPS:
     def play(self):
         while self.roundsRemaining > 0:
             self.askPlayer()
-            print('\n' * 10)
-            self.roundsRemaining -= 1
+            print('\n' * 4) # Space it out a little
+            self.roundsRemaining -= 1 # Decrease the round counter
 
             acceptedAns = False
-            while not acceptedAns:
+            while not acceptedAns: # Loop until a valid answer is given
                 if re.search(f'-{self.player_choice}', '-rock-paper-scissor'):
                     print(self.differenceToWinner(random.randrange(1, 4)))
                     acceptedAns = True
@@ -72,7 +72,7 @@ class RPS:
                 print(f'{self.player_choice} is not a valid response!')
 
 
-def newGame():
+def newGame(): # Set up a new game
     name = input('Hello, my name is Mr. Rock and you are?')
     rounds = int(input(f'Oh, hello {name}! How many rounds would you like to play?'))
 
